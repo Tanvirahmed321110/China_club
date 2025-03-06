a2f13a83ac73d627f17bbcd7606243847807a8c8
tanvir321110 <tanvirahmed321110@gmail.com>
2025-03-06 16:05:35 +0600

    contact page done

---
+++ static/js/common.js
@@ -0,0 +1,19 @@
+
+
+// For Mobile Sidebar
+const menuButton = document.getElementById('mobile-menu-btn')
+const menuSidebar = document.getElementById('mobile-menu-sidebar')
+
+menuButton.addEventListener('click', function () {
+    const menuButtonIcon = menuButton.querySelector('i')
+
+    if (menuButtonIcon.classList.contains('ri-menu-3-fill')) {
+        menuButtonIcon.classList.remove('ri-menu-3-fill')
+        menuButtonIcon.classList.add('ri-close-large-line')
+    } else {
+        menuButtonIcon.classList.remove('ri-close-large-line')
+        menuButtonIcon.classList.add('ri-menu-3-fill')
+    }
+
+    menuSidebar.classList.toggle('active')
+})
